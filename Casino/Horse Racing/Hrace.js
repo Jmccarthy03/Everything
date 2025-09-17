edecb471defb7c1f5921a0e2a256467d44b3dacd
Jmccarthy03 <156253816+Jmccarthy03@users.noreply.github.com>
2025-09-17 13:24:51 -0500

    Update Hrace.js

--- Casino/Horse Racing/Hrace.js
+++ Casino/Horse Racing/Hrace.js
@@ -1,21 +1,48 @@
+
+let bet = document.getElementById('money').value;
+
 function betbutter(){
-  Race()
+  Race();
+  checkbet();
 }
 function race (){
   let winnum = Math.floor(Math.random() * 6) + 1;
     if (winnum == 1)
      let fplace = "Buttercup";
-    cansec = ['Lighting', 'Slowpoke', 'Pegasus', 'Thunderbolt', 'Barry Allen']
+     let cansec = ['Lighting', 'Slowpoke', 'Pegasus', 'Thunderbolt', 'Barry Allen']
      let secnum = Math.floor(Math.random() * cansec.length);
-    runnerup = cansec[secnum];
+     let runnerup = cansec[secnum];
     else if (winnum == 2)
       let fplace = "Lightning";
+      let cansec = ['Buttercup', 'Slowpoke', 'Pegasus', 'Thunderbolt', 'Barry Allen']
+      let secnum = Math.floor(Math.random() * cansec.length);
+      let runnerup = cansec[secnum];
     else if (winnum == 3)
       let fplace = "Slowpoke";
+      let cansec = ['Lighting', 'Buttercup', 'Pegasus', 'Thunderbolt', 'Barry Allen']
+      let secnum = Math.floor(Math.random() * cansec.length);
+      let runnerup = cansec[secnum];
     else if (winnum == 4)
       let fplace = "Pegasus";
+      let cansec = ['Lighting', 'Slowpoke', 'Buttercup', 'Thunderbolt', 'Barry Allen']
+      let secnum = Math.floor(Math.random() * cansec.length);
+      let runnerup = cansec[secnum];
     else if (winnum == 5)
       let fplace = "Thunderbolt";
+      let cansec = ['Lighting', 'Slowpoke', 'Pegasus', 'Buttercup', 'Barry Allen']
+      let secnum = Math.floor(Math.random() * cansec.length);
+      let runnerup = cansec[secnum];
     else if (winnum == 6)
       let fplace = "Barry Allen";
+      let cansec = ['Lighting', 'Slowpoke', 'Pegasus', 'Thunderbolt', 'Buttercup']
+      let secnum = Math.floor(Math.random() * cansec.length);
+      let runnerup = cansec[secnum];
 }
+
+function checkbet(){
+  if (bethorse == fplace) 
+    bet *= 2 
+  if (bethorse == runnerup)
+    bet *= 1.5
+}
+
