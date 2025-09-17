d3e44b8836be0b39de44572a7e0f088e28e7d3d5
Jmccarthy03 <156253816+Jmccarthy03@users.noreply.github.com>
2025-09-17 11:51:23 -0500

    Update Hrace.js

--- Casino/Horse Racing/Hrace.js
+++ Casino/Horse Racing/Hrace.js
@@ -1,1 +1,21 @@
-horse=purple
+function betbutter(){
+  Race()
+}
+function race (){
+  let winnum = Math.floor(Math.random() * 6) + 1;
+    if (winnum == 1)
+     let fplace = "Buttercup";
+    cansec = ['Lighting', 'Slowpoke', 'Pegasus', 'Thunderbolt', 'Barry Allen']
+     let secnum = Math.floor(Math.random() * cansec.length);
+    runnerup = cansec[secnum];
+    else if (winnum == 2)
+      let fplace = "Lightning";
+    else if (winnum == 3)
+      let fplace = "Slowpoke";
+    else if (winnum == 4)
+      let fplace = "Pegasus";
+    else if (winnum == 5)
+      let fplace = "Thunderbolt";
+    else if (winnum == 6)
+      let fplace = "Barry Allen";
+}
